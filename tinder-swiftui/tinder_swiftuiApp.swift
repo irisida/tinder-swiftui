74acb0451fbf01377e237185cc15011a83eddb26
irisida <20445648+irisida@users.noreply.github.com>
2021-05-22 15:58:39 +0100

    Initial Commit

---
+++ tinder-swiftui/tinder_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  tinder_swiftuiApp.swift
+//  tinder-swiftui
+//
+//  Created by ed on 22/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct tinder_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
